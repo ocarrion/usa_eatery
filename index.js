edc4302d7e66f3f5365dff16399b2e05af10c549
ocarrion <71044357+ocarrion@users.noreply.github.com>
2022-04-28 18:14:16 -0500

    updated styles

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementsByClassName("sidebar").style.width = "250px";
+    document.getElementsByClassName("main").style.marginLeft = "250px";
+}
+
+function closeNav(){
+    document.getElementsByClassName("sidenav").style.width = "0";
+    document.getElementsByClassName("main").style.marginLeft = "0";
+}
